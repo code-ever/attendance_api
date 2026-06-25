7df407762b85e39c9fd69bfd7cc2051ff680674c
Code-ever <iykeiykehillary@gmail.com>
2026-06-25 02:53:00 -0700

    Debug Railway environment variables

--- config/db.js
+++ config/db.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 const mysql = require("mysql2");
 
-const db = mysql.createConnection(process.env.MYSQL_URL);
-require("dotenv").config();
+// const db = mysql.createConnection(process.env.MYSQL_URL);
+// require("dotenv").config();
 
 console.log("=== ENV CHECK ===");
 console.log("MYSQLHOST:", process.env.MYSQLHOST);
@@ -13,12 +13,12 @@ console.log("MYSQL_URL:", process.env.MYSQL_URL);
 console.log("DATABASE_URL:", process.env.DATABASE_URL);
 
 process.exit(0);
-db.connect((err) => {
-  if (err) {
-    console.log("Database connection failed:", err);
-  } else {
-    console.log("MySQL Connected Successfully");
-  }
-});
+// db.connect((err) => {
+//   if (err) {
+//     console.log("Database connection failed:", err);
+//   } else {
+//     console.log("MySQL Connected Successfully");
+//   }
+// });
 
-module.exports = db;
+// module.exports = db;
