050e11998a02715cf8600ac4e833359c3a6952ed
Code-ever <iykeiykehillary@gmail.com>
2026-06-25 02:56:47 -0700

    Fix Railway MySQL connection

--- config/db.js
+++ config/db.js
@@ -1,24 +1,20 @@
 require("dotenv").config();
 const mysql = require("mysql2");
 
-// const db = mysql.createConnection(process.env.MYSQL_URL);
-// require("dotenv").config();
+const db = mysql.createConnection({
+  host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+});
 
-console.log("=== ENV CHECK ===");
-console.log("MYSQLHOST:", process.env.MYSQLHOST);
-console.log("MYSQLPORT:", process.env.MYSQLPORT);
-console.log("MYSQLUSER:", process.env.MYSQLUSER);
-console.log("MYSQLDATABASE:", process.env.MYSQLDATABASE);
-console.log("MYSQL_URL:", process.env.MYSQL_URL);
-console.log("DATABASE_URL:", process.env.DATABASE_URL);
+db.connect((err) => {
+  if (err) {
+    console.error("Database connection failed:", err);
+  } else {
+    console.log("MySQL Connected Successfully");
+  }
+});
 
-process.exit(0);
-// db.connect((err) => {
-//   if (err) {
-//     console.log("Database connection failed:", err);
-//   } else {
-//     console.log("MySQL Connected Successfully");
-//   }
-// });
-
-// module.exports = db;
+module.exports = db;
