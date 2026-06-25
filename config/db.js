c2c2c7e6ae2db4ee1160d62d3820c3fcd57319c7
Code-ever <iykeiykehillary@gmail.com>
2026-06-25 02:23:55 -0700

    Fix Railway deployment

--- config/db.js
+++ config/db.js
@@ -18,24 +18,3 @@ db.connect((err) => {
 });
 
 module.exports = db;
-
-
-// const mysql = require("mysql2");
-
-// const db = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "",
-//   database: "student_attendance"
-// });
-
-// db.connect((err) => {
-//   if (err) {
-//     console.log("Database connection failed:", err);
-//   } else {
-//     console.log("MySQL Connected Successfully");
-//   }
-// });
-
-// module.exports = db;
-
