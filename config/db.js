d8f6987cc6905adae0950f9bf8f03b756b206f36
Code-ever <iykeiykehillary@gmail.com>
2026-06-25 02:43:33 -0700

    Debug MYSQL_URL

--- config/db.js
+++ config/db.js
@@ -2,7 +2,16 @@ require("dotenv").config();
 const mysql = require("mysql2");
 
 const db = mysql.createConnection(process.env.MYSQL_URL);
+require("dotenv").config();
 
+console.log({
+  MYSQLHOST: process.env.MYSQLHOST,
+  MYSQLPORT: process.env.MYSQLPORT,
+  MYSQLUSER: process.env.MYSQLUSER,
+  MYSQLDATABASE: process.env.MYSQLDATABASE,
+  MYSQL_URL: process.env.MYSQL_URL,
+  DATABASE_URL: process.env.DATABASE_URL,
+});
 db.connect((err) => {
   if (err) {
     console.log("Database connection failed:", err);
