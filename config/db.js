dc4df0e92b86e26f376ffcb58aa9ea1fa66ef744
Code-ever <iykeiykehillary@gmail.com>
2026-06-25 01:44:37 -0700

    Prepare for Railway deployment

--- config/db.js
+++ config/db.js
@@ -1,10 +1,12 @@
+require("dotenv").config();
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "student_attendance"
+  host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
 });
 
 db.connect((err) => {
@@ -16,3 +18,24 @@ db.connect((err) => {
 });
 
 module.exports = db;
+
+
+// const mysql = require("mysql2");
+
+// const db = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "student_attendance"
+// });
+
+// db.connect((err) => {
+//   if (err) {
+//     console.log("Database connection failed:", err);
+//   } else {
+//     console.log("MySQL Connected Successfully");
+//   }
+// });
+
+// module.exports = db;
+
